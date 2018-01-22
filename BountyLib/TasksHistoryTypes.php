5c2f36b86410b91d7c78d18cbfefef1bce168f7b
Babuka Ivan <babuka19901990@gmail.com>
2018-01-22 14:54:44 +0200

    TaskHistory types

---
+++ BountyLib/TasksHistoryTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BountyLib;
+
+
+class TasksHistoryTypes
+{
+    const CREATE_TASK = 0;
+    const SEND_CONFIRMATION = 1;
+    const APPROVE_TASK = 2;
+    const REJECT_TASK = 3;
+    const SEND_TASK_MESSAGE = 4;
+}
